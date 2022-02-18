3eda09786b33ddd01e4f7b012999bd2c8d6bbe84
Huseyin <81868845+Huseyin-dev@users.noreply.github.com>
2022-02-18 19:59:41 +0200

    Add files via upload

---
+++ typeConversions.py
@@ -0,0 +1,39 @@
+# # str to int
+
+# x = input('1.sayı: ')
+# y = input('2.sayı: ')
+
+# toplam = int(x) + int(y)
+
+# print(toplam)
+
+# #########################
+
+# # int to float
+
+# x = 5
+
+# new_x = float(x)
+
+# print(new_x)
+
+# ########################
+
+# bool to str
+
+# isOnline = True
+
+# isOnline = str(isOnline)
+
+# print(isOnline)
+# print(type(isOnline))
+
+########################
+
+# bool to int
+
+isOnline = False
+
+isOnline = int(isOnline)
+
+print(isOnline)
